0fb0332bc2cb5cfcbdc124d0806c3b0b5ae188da
nathshreya94-ship-it <nathshreya94@gmail.com>
2026-02-09 22:40:32 +0530

    Add files via upload

---
+++ D9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j;
+    char temp;
+
+    scanf("%s", str);
+
+    i = 0;
+    j = strlen(str) - 1;
+    while (i < j) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
