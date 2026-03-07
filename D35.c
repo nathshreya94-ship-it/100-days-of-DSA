8a5f62a7aad9c658dff6f763d5eab577ae149a79
nathshreya94-ship-it <nathshreya94@gmail.com>
2026-03-07 23:41:47 +0530

    Add files via upload

---
+++ D35.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+
+    if (front == -1)
+        front = 0;
+
+    rear++;
+    queue[rear] = value;
+}
+
+void display() {
+    if (front == -1 || front > rear) {
+        printf("Queue is Empty\n");
+        return;
+    }
+
+    for (int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main() {
+    int n, value;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        enqueue(value);
+    }
+
+    display();
+
+    return 0;
+}
